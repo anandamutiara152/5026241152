481cb1bb8c27a0983c173f72889c785adb3f5e30
anandamutiara152 <5026241152@its.student.its.ac.id>
2026-04-26 15:07:02 +0700

    tugas pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,41 @@ Route::get('pert5', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('pert5index', function () {
+	return view('indexio');
+});
+
+Route::get('tugaspertemuan5', function () {
+	return view('linktree');
+});
+
+Route::get('tugaspertemuan4', function () {
+	return view('grid');
+});
+
+Route::get('tugaspertemuan3', function () {
+	return view('thestoryof');
+});
+
+Route::get('pertemuan1', function () {
+	return view('intro');
+});
+
+Route::get('news', function () {
+	return view('news');
+});
+
+Route::get('news1', function () {
+	return view('news1');
+});
+
+Route::get('responsive', function () {
+	return view('responsive');
+});
+Route::get('template', function () {
+	return view('template');
+});
+
+
+
