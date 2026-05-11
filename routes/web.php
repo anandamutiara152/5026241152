40d6cd35b3f14183e94d46976bc6053bf20482b8
anandamutiara152 <5026241152@its.student.its.ac.id>
2026-05-11 12:05:58 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
 
@@ -25,13 +26,15 @@ Route::get('pert5', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/pegawainama/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 //blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
 
 Route::get('pert5index', function () {
 	return view('indexio');
